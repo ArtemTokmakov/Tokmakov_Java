530d5547980a214c1932595b278f396a577fb2f0
Артём Токмаков <temtokmakov@yandex.ru>
2024-02-07 18:41:44 +0300

    first

---
+++ CompareNumbers.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class CompareNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int b = scanner.nextInt();
+        if (a > b) {
+            System.out.println("a > b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a = b");
+        }
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        double quotient = (double) a / b;
+
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + product);
+        System.out.println("Частное: " + quotient);
+
+        scanner.close();
+    }
+}
